e2d896c79c0c9e316fd9b78a4830a2c4de29b4eb
drsn-ajani <daajani9103@gmail.com>
2024-12-22 20:24:27 +0530

    Add: UC3 Functionality (No play, Ladder, Snake)

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -5,14 +5,37 @@ public class SnakeAndLadder {
         this.player_position = 0;
     }
 
-//    UC-1
+//    UC-1      -->     Displaying Initial player position
     public void diplay() {
         System.out.println("Initial Position of Player is: " + player_position);
     }
 
-//    UC-2
+//    UC-2      -->     Rolling the Dice
     public int roll_the_dice() {
         return (int) Math.floor(Math.random() * 6) + 1;
+    }
+
+//    UC-3      -->     Looking for options {0: No play, 1: Ladder(Move forward), 2: Snake(Move Backward)}
+    public void set_player_position() {
+        int num = (int) Math.floor(Math.random() * 3);
+
+        int roll = roll_the_dice();
+
+        switch (num) {
+            case 0:
+                System.out.println("Player will stay at the same position");
+                break;
+            case 1:
+                player_position += roll;
+                System.out.println("Player will move in Forward Dir with " + roll + " steps\tWill reach at " + player_position);
+                break;
+
+            case 2:
+                player_position -= roll;
+                System.out.println("Player will move in Backward Dir with " + roll + " steps\tWill reach at " + player_position);
+                break;
+
+        }
     }
 
     public static void main(String[] args) {
@@ -23,7 +46,10 @@ public class SnakeAndLadder {
         game.diplay();
 
 //        UC-2
-        System.out.println("Number got on rolling the dice is: " + game.roll_the_dice());
+//        System.out.println("Number got on rolling the dice is: " + game.roll_the_dice());
+
+//        UC-3
+        game.set_player_position();
 
     }
 
